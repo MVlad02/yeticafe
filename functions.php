d27ea7f6993c0a990f50ef76011a15c57769e94d
sva01023 <105356110+MVlad02@users.noreply.github.com>
2022-05-24 14:07:36 +0300

    Практическая 5

---
+++ functions.php
@@ -0,0 +1,33 @@
+<?php
+
+function price ($price)
+{
+    $price =ceil($price);
+    if ($price >= 1000){
+        $result = number_format($price,0,'.',' ');
+    }
+    else $result = $price;
+    return $result . ' ₽';
+}
+function timer()
+{
+    $date= strtotime('2022-05-12 24:00');
+    $ntime= time();
+    $difference= $date-$ntime;
+    return gmdate('H:i', $difference);
+}
+function include_template($name, $data)
+{
+    $name = 'templates/' . $name;
+    $result = '';
+    if (!file_exists($name))
+    {
+        return $result;
+    }
+    ob_start();
+    extract($data);
+    require($name);
+    $result = ob_get_clean();
+    return $result;
+}
+?>
